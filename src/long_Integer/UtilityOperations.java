60553089750240a4035260d7a59a026c0b87bdc1
Dakota <blackhawk101x@gmail.com>
2016-10-17 12:14:03 -0400

    Initial setup of the Utility Class

---
+++ src/long_Integer/UtilityOperations.java
@@ -0,0 +1,21 @@
+package long_Integer;
+
+/**
+ * Support operations for the linked list
+ * @author Dakota Buell
+ *
+ */
+public class UtilityOperations {
+
+	public static int overFlow(int t) {
+		  return -1;
+    }
+
+    public static int underFlow(int t) {
+      return -1;
+    }
+
+    public static int digits(int t) {
+      return -1;
+    }
+}
